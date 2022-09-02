841aa4301c21c05a61e61957d8a62d8d3ecf7305
吴成义 <chengyiwu@91jkys.com>
2022-09-02 10:39:58 +0800

    add yaml to json

---
+++ README.md
@@ -0,0 +1,30 @@
+yaml2json
+=========  
+
+[![Build Status](https://img.shields.io/github/workflow/status/dafu-wu/yaml2json/CI/main)](https://github.com/dafu-wu/yaml2json/actions?query=workflow%3ACI)
+[![Crates.io](https://img.shields.io/crates/v/yaml2json)](https://crates.io/crates/yaml2json)
+
+A command-line tool that converts YAML to JSON.
+
+
+## Installation
+
+### Cargo
+
+```
+$ cargo install yaml2json
+```
+
+## Usage
+
+Convert YAML on `stdin` to JSON, filtering it through `jq`:
+
+```bash
+$ yaml2json <<< 'okay = "ok"' | jq
+```
+
+Convert YAML from a file and pretty-print it without `jq`:
+
+```bash
+$ yaml2json --pretty ~/.cargo/.crates.toml
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::{self, Read};
+
+use anyhow::{Context, Result};
+use clap::{Arg, Command};
+
+fn main() -> Result<()> {
+    let app = Command::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::new("pretty")
+                .help("pretty print the JSON")
+                .short('p')
+                .long("pretty"),
+        )
+        .arg(
+            Arg::new("input")
+                .help("the YAML to convert")
+                .index(1)
+                .default_value("-"),
+        );
+    let matches = app.get_matches();
+    let input_src = matches.value_of("input").unwrap();
+    let input_buf = match input_src {
+        "-" => {
+            let mut input_buf = String::new();
+            io::stdin()
+                .read_to_string(&mut input_buf)
+                .with_context(|| "failed to collect stdin")?;
+            input_buf
+        }
+        input => fs::read_to_string(input)
+            .with_context(|| format!("failed to collect from input: {}", input))?,
+    };
+
+    let value = serde_yaml::from_str::<serde_json::Value>(&input_buf)
+        .with_context(|| format!("parsing YAML from {} failed", input_src))?;
+
+    if matches.is_present("pretty") {
+        serde_json::to_writer_pretty(io::stdout(), &value)
+    } else {
+        serde_json::to_writer(io::stdout(), &value)
+    }
+    .with_context(|| "JSON serialization and/or stdout streaming failed")?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_app() {
+        assert!(true)
+    }
 }
